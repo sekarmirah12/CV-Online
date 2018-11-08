7860a97b2ab03e05435dee30b30ec3c56a967f36
ekojumaddin <33531912+ekojumaddin@users.noreply.github.com>
2018-11-08 15:20:02 +0800

    Add files via upload

--- CvOnline/CvOnline/ClientHome.xaml.cs
+++ CvOnline/CvOnline/ClientHome.xaml.cs
@@ -27,10 +27,14 @@ namespace CV_Online
         Company company = new Company();
         Job_Detail jobdetail = new Job_Detail();
         Provinces province = new Provinces();
+        Client client = new Client();
+        Language language = new Language();
+        Experience experience = new Experience();
+        Skill skill = new Skill();
         public ClientHome()
         {
             InitializeComponent();
-            
+            TampilPos();
         }
 
         //public void LoadDataJobDetail()
@@ -138,6 +142,120 @@ namespace CV_Online
             }
         }
 
-       
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            client.Collage = txtCollege.Text;
+            client.Departement = txtDepartment.Text;
+            client.CV = txtCV.Text;
+            client.KTP = txtKTP.Text;
+            client.Last_Education = txtEducation.Text;
+            client.Photo = txtPhoto.Text;
+            context.Clients.Add(client);
+            context.SaveChanges();
+        }
+
+        private void btnPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            Stream checkStream = null;
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = false;
+            dlg.Filter = "All Image Files | *.*";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                try
+                {
+                    if ((checkStream = dlg.OpenFile()) != null)
+                    {
+                        string filename = dlg.FileName;
+                        txtPhoto.Text = filename;
+                        MessageBox.Show("Successfully done", filename);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Problem occured, try again later");
+            }
+        }
+
+        private void dataGridExperience_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object exp = dataGridExperience.SelectedItem;
+            cmbPosition.Text = (dataGridExperience.SelectedCells[2].Column.GetCellContent(exp) as TextBlock).Text;
+            //cmbCompany.Text = (dataGridExperience.SelectedCells[3].Column.GetCellContent(exp) as TextBlock).Text;
+            txtLength.Text = (dataGridExperience.SelectedCells[4].Column.GetCellContent(exp) as TextBlock).Text;
+        }
+
+        private void dataGridLanguage_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            object language = dataGridLanguage.SelectedItem;
+            txtLanguage.Text = (dataGrid.SelectedCells[0].Column.GetCellContent(language) as TextBlock).Text;
+        }
+
+        private void cmbPosition_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public void TampilPos()
+        {
+            //int id = Convert.ToInt16(cmbPosition.SelectedValue);
+            //var getDataPosition = context.Experiences.Where(x => x.Position.Id == id).ToList();
+
+            //cmbPosition.ItemsSource = getDataPosition;
+
+            try
+            {
+                int id = Convert.ToInt16(cmbPosition.SelectedValue);
+                var getData = context.Positions.ToList();
+                cmbPosition.ItemsSource = getData;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+
+        }
+
+        private void btnAddExp_Click(object sender, RoutedEventArgs e)
+        {
+            dataGridExperience.Items.Add(
+            new
+            {
+            Position_Id = cmbPosition.SelectedValue,
+            //Company_Id = cmbCompany.SelectedValue,
+            Name = cmbPosition.Text,
+            //Name = cmbCompany.Text,
+            JobPosition = cmbPosition.Text,
+            //CompanyName = cmbCompany.Text,
+            Length = txtLength.Text
+            });
+        }
+
+        //private void btnAddLang_Click(object sender, RoutedEventArgs e)
+        //{
+        //    language.Name = txtLanguage.Text;
+        //    //context.Languages.Add(language);
+        //    //context.SaveChanges();
+        //    //LoadDataLanguage();
+
+        //    try
+        //    {
+        //        context..Add(language);
+        //        context.SaveChanges();
+        //        LoadDataLanguage();
+        //    }
+
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.InnerException);
+        //    }
+        //}
     }
 }

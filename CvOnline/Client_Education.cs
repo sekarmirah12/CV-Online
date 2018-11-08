170d55ff05698988affbf4220c3044f74f6cc349
sekarmirah12 <sekarmirah012@gmail.com>
2018-11-08 16:01:37 +0700

    model

---
+++ CvOnline/Client_Education.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CvOnline.MyModel
+{
+    public class Client_Education : BaseModel
+    {
+        public Client clients { get; set; }
+        public Education educations { get; set; }
+    }
+}
